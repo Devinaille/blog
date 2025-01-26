19f4b258d203d7f89c11e612fa0e7df24cad64cc
滿開 <devinailletian@163.com>
2025-01-26 00:29:12 +0000

    初始化docsify文档

--- src/static/js/custom.js
+++
@@ -1,4 +0,0 @@
-var beian = "<div style="display:flex;">
-    <img src="/static/logo/beian.png"></img>
-    <a href="https://beian.mps.gov.cn/#/query/webSearch?code=33010902004053" rel="noreferrer" target="_blank">浙公网安备33010902004053号</a>
-</div>""
